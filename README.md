2515ff960ced250c67bc93f874a7f96cad4c21e7
Dave Sanders <davepaulsanders@gmail.com>
2022-05-25 10:19:07 -0400

    Adding comments and updating readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# NBA Standings Scraper
+
+After realizing an API I was using did not have access to wins or standings, I developed this app as a module to include in my other NBA data project.
+
+## Installation
+
+1. `git clone` the repository
+2. `npm install` for dependencies
+3. `npm start` to start app in node
+
+## Technologies
+
+This app uses puppeteer, which you can read about [here.](https://pptr.dev/)
--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const getStandings = async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
     await page.goto("https://www.cbssports.com/nba/standings/");
-    // Getting team abbreviations from ESPN
+    
+    // Getting teams from CBS
     let teams = await page.$$eval("span a", (teams) =>
       teams.map((team) => {
         return team.textContent;
@@ -14,15 +14,21 @@ const getStandings = async () => {
     );
 
     await browser.close();
+
+    // removing extra data included during selection
     teams = teams.filter(
       (team) => team !== "" && team !== "Promoted by Taboola"
     );
+
+    // splitting into conferences and adding number ranking
     const Eastern = teams.splice(0, 15).map((team, index) => {
       return `${index + 1}: ${team}`;
     });
     const Western = teams.map((team, index) => {
       return `${index + 1}: ${team}`;
     });
+
+    // These values could also be returned as a module
     console.log(Eastern);
     console.log(Western);
   } catch (e) {
